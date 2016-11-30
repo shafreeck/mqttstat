ae88eb67c5f5dd161ef447c1475ffcac0e8ebaf2
任勇全 <ryq@meitu.com>
2016-12-01 03:49:43 +0800

    Remove unused files

--- mqtt/hook.go
+++
@@ -1,7 +0,0 @@
-package mqtt
-
-type Hook struct {
-	ConnectHook   func() error
-	SubscribeHook func(topics []string, qoss []int) error
-	PublishHook   func(topic string, message []byte, qos int)
-}
